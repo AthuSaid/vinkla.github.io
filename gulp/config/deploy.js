7ffc38663cf48a613c5e85382ba1c33439f33c59
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-21 12:27:45 +0100

    Update 2015-11-21T11:27:43.191Z

--- gulp/config/deploy.js
+++
@@ -1,9 +0,0 @@
-import config from './';
-
-export default {
-  url: 'https://vinkla.com/',
-  src: config.public + '/**/*',
-  ghPages: {
-    branch: master
-  }
-};
--- gulp/tasks/markup.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
-import config from '../config/markup';
-
-gulp.task('markup', () => {
-  return gulp.src(config.src)
-    .pipe(htmlmin(config.settings))
-    .pipe(gulp.dest(config.dest))
-});
