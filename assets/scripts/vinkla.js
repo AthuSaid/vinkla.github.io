3001909f70e6d7fe6ee18f67f2fa508550a0a9c7
Vincent Klaiber <vincentklaiber@gmail.com>
2016-10-14 16:19:21 +0200

    Fix the order

--- assets/scripts/vinkla.js
+++ assets/scripts/vinkla.js
@@ -1,23 +1,15 @@
 'use strict';
 
 if ('speechSynthesis' in window) {
+  console.log('https://en.m.wikipedia.org/wiki/Konami_Code');
+  
   const songs = [
     'The power of love, is a curious thing. Make a one man weep, make another man sing. Change a hawk to a little white dove. More than a feeling that\'s the power of love.',
-    'Never gonna give you up, never gonna let you down. Never gonna run around and desert you. Never gonna make you cry, never gonna say goodbye. Never gonna tell a lie and hurt you.'
+    'Never gonna give you up, never gonna let you down. Never gonna run around and desert you. Never gonna make you cry, never gonna say goodbye. Never gonna tell a lie and hurt you.',
+    'Walking like a man. Hitting like a hammer. She\'s a juvenile scam. Never was a quitter. Tasty like a raindrop. She\'s got the look.',
   ];
 
-  let keys = [
-    'ArrowUp',
-    'ArrowUp',
-    'ArrowDown',
-    'ArrowDown',
-    'ArrowLeft',
-    'ArrowRight',
-    'ArrowLeft',
-    'ArrowRight',
-    'a',
-    'b',
-  ];
+  let keys = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 
   let index = 0;
 
