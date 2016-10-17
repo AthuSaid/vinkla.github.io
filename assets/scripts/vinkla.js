2c9ef7e94eabf5267610bd7e3ba7f0a25b8a1f90
Vincent Klaiber <vincentklaiber@gmail.com>
2016-10-17 10:57:06 +0200

    Add konami code

--- assets/scripts/vinkla.js
+++ assets/scripts/vinkla.js
@@ -2,7 +2,7 @@
 
 if ('speechSynthesis' in window) {
   console.log('https://en.m.wikipedia.org/wiki/Konami_Code');
-  
+
   const songs = [
     'The power of love, is a curious thing. Make a one man weep, make another man sing. Change a hawk to a little white dove. More than a feeling that\'s the power of love.',
     'Never gonna give you up, never gonna let you down. Never gonna run around and desert you. Never gonna make you cry, never gonna say goodbye. Never gonna tell a lie and hurt you.',
@@ -13,15 +13,27 @@ if ('speechSynthesis' in window) {
 
   let index = 0;
 
+  let konami = document.querySelector('.konami')
+  let elements = konami.querySelectorAll('kbd')
+
   window.addEventListener('keydown', function(event) {
     window.speechSynthesis.cancel();
 
     if (keys[index] === event.key) {
-      console.log(keys[index++]);
+      konami.classList.add('active');
+
+      index += 1;
+
+      elements[index - 1].classList.add('active');
     }
 
     if (keys.length === index) {
       index = 0;
+
+      setTimeout(function() {
+        konami.classList.remove('active');
+        elements.forEach(element => element.classList.remove('active'));
+      }, 2000);
 
       const lyrics = songs[Math.round(Math.random() * (songs.length - 1))];
 
