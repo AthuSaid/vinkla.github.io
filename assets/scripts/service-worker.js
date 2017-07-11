d743db514b9cd3f4ecaaf2a8deb0f18fb31aaed4
Vincent Klaiber <vincentklaiber@gmail.com>
2017-07-11 11:24:09 +0200

    Update icons

--- assets/scripts/service-worker.js
+++ assets/scripts/service-worker.js
@@ -6,7 +6,6 @@ const VERSION = 'cache-v{{site.time | replace:" ","" | replace:":","" | replace:
 
 const urls = [
   '/',
-  '/favicon.ico',
   '/robots.txt',
   '/manifest.json',
 
@@ -14,6 +13,7 @@ const urls = [
   '/assets/images/chrome-touch-icon-192x192.png',
   '/assets/images/icon-128x128.png',
   '/assets/images/ms-touch-icon-144x144-precomposed.png',
+  '{{ site.logo }}',
 
   'https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css',
   '/assets/styles/vinkla.css',
